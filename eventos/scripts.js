df7332d431e9383d715cc44a3ad41f31bd52fbad
Rafael Lima Dalmagro <rafa.dalmagro@hotmail.com>
2025-07-15 17:20:09 -0300

    Manipulação de eventos

---
+++ eventos/scripts.js
@@ -0,0 +1,37 @@
+// const ul = document.querySelector("ul");
+
+// ul.addEventListener("scroll", () => {
+// 	// console.log(ul.scrollTop);
+
+// 	if(ul.scrollTop > 300){
+// 		console.log("FIM DA LISTA");
+
+// 		ul.scrollTo({
+// 			top:0,
+// 			behavior: "smooth",
+// 		})
+// 	}
+// });
+
+// const button = document.querySelector("button")
+
+// button.addEventListener("click", (event)=>{
+// 	event.preventDefault()
+// 	console.log("CLICOU");
+// })
+
+// const form = document.querySelector("form");
+// form.onsubmit = (event) => {
+//     event.preventDefault();
+// 	console.log("Você submitou")
+// };
+
+// const input = document.querySelector("input")
+
+// input.addEventListener("keydown", (event)=>{
+// 	console.log(event);
+// })
+
+// input.addEventListener("keypress", (event)=>{
+// 	console.log(event.code);
+// })
