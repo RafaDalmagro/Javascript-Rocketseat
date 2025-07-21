718fc1e1d332195eb92ff97147c9b13322632677
Rafael Lima Dalmagro <rafa.dalmagro@hotmail.com>
2025-07-21 13:31:39 -0300

    Importação e exportações.

---
+++ JavascriptModerno/Modules/calc.js
@@ -0,0 +1,21 @@
+export class Calculator {
+    sum(a, b) {
+        return a + b;
+    }
+
+    subtract(a, b) {
+        return a - b;
+    }
+}
+
+// export function sum(a, b) {
+//     return a + b;
+// }
+
+// function subtract(a, b) {
+//     return a - b;
+// }
+
+// export {
+// 	Calculator
+// }
---
+++ JavascriptModerno/Modules/main.js
@@ -0,0 +1,5 @@
+import { Calculator } from "./calc";
+
+const calculator = new Calculator();
+
+console.log(`O resultado da subtração de 5 - 3 é = ${calculator.subtract(5, 3)}`); // Outputs: 2
