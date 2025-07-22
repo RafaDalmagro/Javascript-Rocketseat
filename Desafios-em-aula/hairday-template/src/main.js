613dd1c83e1254c40e46386d29feeb888ddeb2a5
Rafael Lima Dalmagro <rafa.dalmagro@hotmail.com>
2025-07-22 08:34:33 -0300

    Configurações dos pacotes.

---
+++ Desafios-em-aula/hairday-template/src/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// Configuração do Day.js para o idioma português
+import "./lib/dayjs";
+
+//CSS
+
+import "./styles/global.css";
+import "./styles/form.css";
+import "./styles/schedule.css";
+
